451cf7ef205bddab6835b2b9799b6866e3a7ede1
Silvio Fragnani <silviofragnanisilva@gmail.com>
2021-04-26 22:17:26 +0200

    circleci setup

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,7 +8,7 @@ plugins {
 }
 
 group = "com.cv"
-version = "0.0.1-SNAPSHOT"
+version = System.getenv("TAG") ?: "0.0.1-SNAPSHOT"
 java.sourceCompatibility = JavaVersion.VERSION_1_8
 
 configurations {
